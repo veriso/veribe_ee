09d04563dee07007a95c8648d3d7839bae579ed8
hka <hka@sourcepole.ch>
2022-02-15 19:23:04 +0100

    Add forest defects type

--- applicationmodule.py
+++ applicationmodule.py
@@ -2,7 +2,11 @@
 
 from veriso.base.utils.utils import dynamic_import
 from veriso.modules.applicationmodule_base import ApplicationModuleBase
-from veriso.base.utils.utils import tr
+from qgis.PyQt.QtWidgets import QMenu, QMenuBar
+from qgis.PyQt.QtCore import QCoreApplication, QTranslator, QSettings
+
+
+import os
 
 
 class ApplicationModule(ApplicationModuleBase):
@@ -12,12 +16,44 @@ class ApplicationModule(ApplicationModuleBase):
 
     def __init__(self, veriso):
         super(ApplicationModule, self).__init__(veriso)
+        self.module_dir = os.path.dirname(__file__)
+        # self.load_module_translations()
+        self.do_init_forest_defects_menu()
 
-    def do_load_defects(self):
+    def load_module_translations(self):
+        """Load the modules translations
+        """
+        locale = QSettings().value('locale/userLocale')[0:2]
+        self.translator = QTranslator()
+        self.translator.load(
+            os.path.join(
+                self.module_dir,
+                'i18n',
+                'veribe_ee_{}.qm'.format(locale)))
+        QCoreApplication.installTranslator(self.translator)
+
+    def do_init_forest_defects_menu(self):
+        menubar = self.toolbar.findChild(
+            QMenuBar, 'VeriSO.Main.LoadDefectsMenuBar')
+        # Specialized defects menu only for forest points
+        menu = QMenu(menubar)
+        menu.setObjectName("VeriSO.Main.LoadForestDefectsMenu")
+        menu.setTitle(self.tr("Forest Defects"))
+        # Add defects dock action from default defects menu
+        default_menu = menubar.findChild(QMenu, 'VeriSO.Main.LoadDefectsMenu')
+        for action in default_menu.actions():
+            if action.objectName() == "VeriSO.Main.LoadDefectsMenuBar.ShowDock":
+                menu.addAction(action)
+                break
+
+        self.add_defects_actions(menu, "forest")
+        menubar.addMenu(menu)
+
+    def do_load_defects(self, defects_type):
         defects_module = 'veriso.modules.loaddefects_base'
         defects_module = dynamic_import(defects_module)
         tr_tag = self.module_name
-        d = defects_module.LoadDefectsBase(self.iface, tr_tag)
+        d = defects_module.LoadDefectsBase(self.iface, tr_tag, defects_type)
 
         fields = {
             'ogc_fid': {'widget': 'TextEdit',
@@ -71,81 +107,148 @@ class ApplicationModule(ApplicationModuleBase):
                 'config': {
                     'CheckedState': 't',
                     'UncheckedState': 'f'},
-                'writable_only_by': ['agi']}}
+                'writable_only_by': ['agi']}
+        }
+
+        forest_fields = {
+            'ogc_fid': {'widget': 'TextEdit',
+                        'readonly': True,
+                        'config': {"Editable": False}},
+            'bezeichnung': {
+                'widget': 'Enumeration',
+                'alias': self.tr('Bezeichnung:'),
+                'writable_only_by': ['agi', 'avor']},
+            'bem': {
+                'widget': 'TextEdit',
+                'alias': self.tr('Bemerkung:'),
+                'config': {"IsMultiline": True},
+                'writable_only_by': ['agi', 'avor']},
+            'datum': {'widget': 'Hidden'},
+            'bem_forst': {
+                'widget': 'TextEdit',
+                'alias': self.tr('Bemerkung Forst:'),
+                'config': {"IsMultiline": True},
+                'writable_only_by': ['agi', 'forst']},
+        }
 
         d.layers = {
-            'point': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Punkte)", tr_tag, None),
-                "featuretype": "t_maengel_punkt",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_punkte.qml",
-                "fields": fields
+            'default': {
+                'point': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Punkte)"),
+                    "featuretype": "t_maengel_punkt",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_punkte.qml",
+                    "fields": fields
                 },
-            'pointdesc': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Punkte) Beschreibung", tr_tag, None),
-                "featuretype": "t_maengel_punkt",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_punkte_beschreibung.qml",
-                "fields": fields
+                'pointdesc': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Punkte) Beschreibung"),
+                    "featuretype": "t_maengel_punkt",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_punkte_beschreibung.qml",
+                    "fields": fields
                 },
-            'line': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Linien)", tr_tag, None),
-                "featuretype": "t_maengel_linie",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_linien.qml",
-                "fields": fields
+                'line': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Linien)"),
+                    "featuretype": "t_maengel_linie",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_linien.qml",
+                    "fields": fields
                 },
-            'linedesc': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Linien) Beschreibung", tr_tag, None),
-                "featuretype": "t_maengel_linie",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_linien_beschreibung.qml",
-                "fields": fields
+                'linedesc': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Linien) Beschreibung"),
+                    "featuretype": "t_maengel_linie",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_linien_beschreibung.qml",
+                    "fields": fields
                 },
-            'polygon': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Polygone)", tr_tag, None),
-                "featuretype": "t_maengel_polygon",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_polygone.qml",
-                "fields": fields
+                'polygon': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Polygone)"),
+                    "featuretype": "t_maengel_polygon",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_polygone.qml",
+                    "fields": fields
                 },
-            'polygondesc': {
-                "type": "postgres",
-                "title": tr(u"Mängelliste (Polygone) Beschreibung", tr_tag, None),
-                "featuretype": "t_maengel_polygon",
-                "geom": "the_geom",
-                "key": "ogc_fid",
-                "readonly": True,
-                "sql": "",
-                "group": d.group,
-                "style": "maengel/maengelliste_polygone_beschreibung.qml",
-                "fields": fields
+                'polygondesc': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Polygone) Beschreibung"),
+                    "featuretype": "t_maengel_polygon",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_polygone_beschreibung.qml",
+                    "fields": fields
+                }
+            },
+            'forest': {
+                'point': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Punkte)"),
+                    "featuretype": "t_forest_maengel_punkt",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_punkte.qml",
+                    "fields": forest_fields
+                },
+                'pointdesc': {
+                    "type": "postgres",
+                    "title": self.tr("Mängelliste (Punkte) Beschreibung"),
+                    "featuretype": "t_forest_maengel_punkt",
+                    "geom": "the_geom",
+                    "key": "ogc_fid",
+                    "readonly": True,
+                    "sql": "",
+                    "group": d.group,
+                    "style": "maengel/maengelliste_punkte_beschreibung.qml",
+                    "fields": forest_fields
                 }
             }
+        }
 
         return d.run()
+
+    def get_defects_table_names(self):
+        """Return the table names of the defects tables
+
+        Returns:
+            dict: dict containing the default table names
+        """
+        return {
+            'default': {
+                "point": "t_maengel_punkt",
+                "line": "t_maengel_linie",
+                "polygon": "t_maengel_polygon"
+            },
+            'forest': {
+                "point": "t_forest_maengel_punkt"
+            }
+        }
